87800dd8385ba01a65a2e7ca6162701b0c530e6e
25aullal <25aullal@derryfield.org>
2024-10-25 20:16:17 -0400

    Finished Lesson 09

--- Loops09/Loops09C.java
+++ Loops09/Loops09C.java
@@ -4,11 +4,19 @@ public class Loops09C {
     public static void main(String[] args) {
         int sum = 0;
         for (int i = 1; i<=10000000; i++) {
-            int r = (int)Math.round(Math.random()*10);
-            System.out.print(r);
+            int r = (int)(Math.random()*10 + 1);
+            if (r>=1 && r<=10) {
+                System.out.println(r);
             sum += r;
+            } else {
+                i--;
+            }
+            
         }
         double avg = sum/10000000.0;
         System.out.printf("The average is %.2f", avg);
     }
 }
+
+//The average of near 5 makes sense. (1+10)/2 = 5.5
+//Increasing loops# causes the average to approach 5.50 consistently.
---
+++ Loops09/Loops09D_AT.java
@@ -0,0 +1,89 @@
+package Loops09;
+import java.util.Scanner;
+
+public class Loops09D_AT {
+    int dice_roll;
+    void rollDice() {
+        boolean loopFlag = true;
+        do {
+            dice_roll = (int)(1+(Math.random()*6));
+
+            if (dice_roll >= 1) {
+                loopFlag = false;
+            }
+        } while (loopFlag);
+    }
+
+    int viewDice() {
+        return dice_roll;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int i = 0;
+        Loops09D_AT dice = new Loops09D_AT();
+        while (i == 0) {
+            System.out.print("Enter 'r' to roll, 'd' to display, or 'q' to quit: ");
+            String action = (in.nextLine()).toUpperCase();
+
+            if (action.equals("R")) {
+                dice.rollDice();
+            }
+
+            if (action.equals("Q")) {
+                i++;
+            }
+
+            if (action.equals("D")) {
+                if (dice.viewDice()==0) {
+                    System.out.println("Please roll the dice first.");
+                } else if (dice.viewDice()==1) {
+                    System.out.print("+-------+\n" + //
+                                                "|       |\n" + //
+                                                "|   0   |\n" + //
+                                                "|       |\n" + //
+                                                "+-------+\n" + //
+                                                "");
+                } else if (dice.viewDice()==2) {
+                    System.out.print("+-------+\n" + //
+                                                "| 0     |\n" + //
+                                                "|       |\n" + //
+                                                "|     0 |\n" + //
+                                                "+-------+\n" + //
+                                                "");
+                } else if (dice.viewDice()==3) {
+                    System.out.print("+-------+\n" + //
+                                                "| 0     |\n" + //
+                                                "|   0   |\n" + //
+                                                "|     0 |\n" + //
+                                                "+-------+\n" + //
+                                                "");
+                } else if (dice.viewDice()==4) {
+                    System.out.print("+-------+\n" + //
+                                                "| 0   0 |\n" + //
+                                                "|       |\n" + //
+                                                "| 0   0 |\n" + //
+                                                "+-------+\n" + //
+                                                "");
+                } else if (dice.viewDice()==5) {
+                    System.out.print("+-------+\n" + //
+                                                "| 0   0 |\n" + //
+                                                "|   0   |\n" + //
+                                                "| 0   0 |\n" + //
+                                                "+-------+\n" + //
+                                                "");
+                } else if (dice.viewDice()==6) {
+                    System.out.print("+-------+\n" + //
+                                                "| 0   0 |\n" + //
+                                                "| 0   0 |\n" + //
+                                                "| 0   0 |\n" + //
+                                                "+-------+\n" + //
+                                                "");
+                }
+
+            }
+        }
+    }
+
+    
+}
