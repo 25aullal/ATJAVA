b53848b3b828e8c563f7990a12c413f2035a70de
25aullal <25aullal@derryfield.org>
2024-11-19 11:00:22 -0500

    stuffinclass

---
+++ Lesson 14/LargestNum.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class LargestNum {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+       
+        double val = 0;
+        double largest = Double.MIN_VALUE;
+        double[] values = new double[100];
+        double sum = 0;
+        int count = 0;
+        for (int i = 0; i < values.length; i++) {
+            System.out.print("Enter a test score: ");
+            if (in.hasNext("Q") || in.hasNext("q")) {
+                in.next();
+                break;
+            }
+            val = in.nextDouble();
+            if (val >= 0 && val <= 150) {
+                
+                values[i] = val;
+            
+                if (val > largest) {
+                   largest = val;
+                }
+                sum += val;
+                count ++;
+            } else {
+                i--;
+            }
+        }
+        System.out.println("Capacity full, or input done.");
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] == largest) {
+                System.out.println(values[i] + " <= largest");
+            } else if (values[i] == 0) {
+            } else {
+                System.out.println(values[i]);
+            }
+        }
+        double average = sum / (double)count;
+        System.out.println("Average = " + average);
+    }
+}
---
+++ Lesson 14/Numbers.java
@@ -0,0 +1,8 @@
+public class Numbers {
+    public static void main(String[] args) {
+        double[] numbers = {8, 6, 7, 5, 3, 0, 9};
+        for (double elements : numbers) {
+            System.out.println(elements);
+        }
+    }
+}
