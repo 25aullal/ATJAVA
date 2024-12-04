9eaaeddc678339a300cf16c5cd39a88fc3a8ed7d
25aullal <25aullal@derryfield.org>
2024-12-03 20:57:13 -0500

    Completing Assignment

--- Lesson 14/Ignore_LargestNum.java
+++ Lesson 14/Ignore_LargestNum.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
-public class LargestNum {
+public class Ignore_LargestNum {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
        
         double val = 0;
         double largest = Double.MIN_VALUE;
-        double[] values = new double[100];
+        double[] values = new double[5];
         double sum = 0;
         int count = 0;
         for (int i = 0; i < values.length; i++) {
-            System.out.print("Enter a test score: ");
+            System.out.print("Enter a test score, or q to quit: ");
             if (in.hasNext("Q") || in.hasNext("q")) {
                 in.next();
                 break;
@@ -42,3 +42,8 @@ public class LargestNum {
         System.out.println("Average = " + average);
     }
 }
+
+
+//filling an array: int a[] = new int[5]
+//for int i=0; i<a.length;i++
+    //a[i] = i
---
+++ Lesson 14/Lesson14B.java
@@ -0,0 +1,14 @@
+public class Numbers {
+    public static void main(String[] args) {
+        int count = 0;
+        double total = 0.0;
+        double[] numbers = {2.2, 1.0, 3.7, 4.1, 88.0};
+        for (double elements : numbers) {
+            System.out.println(elements);
+            count++;
+            total+= elements;
+        }
+        double avg = total/count;
+        System.out.println("Avg = " + avg);
+    }
+}
