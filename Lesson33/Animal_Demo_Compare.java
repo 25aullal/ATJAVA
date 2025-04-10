05bda9fbfa69d921dfd21e9d23e7ba027d9841ee
25aullal <25aullal@derryfield.org>
2025-04-09 21:24:49 -0400

    No2 W

--- Lesson33/Animal_Demo_Compare.java
+++ Lesson33/Animal_Demo_Compare.java
@@ -1,8 +1,8 @@
 public class Animal_Demo_Compare {
 
     public static void main(String[] args) {
-        Comparable<Animal> kangarooster = new Animal("Kangarooster", 45.5);
-        Animal hippopotter = new Animal("Hippopotter", 500.0);
+        Comparable<Animal> kangarooster = new Animal("Kangarooster", 45.49999999);
+        Animal hippopotter = new Animal("Hippopotter", 45.5);
 
         if (kangarooster.compareTo(hippopotter) == -1) {
             System.out.println("kangarooster is lighter");
