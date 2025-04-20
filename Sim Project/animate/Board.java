43c5a248b27b731c783e6936d2ead72b5d78492d
25aullal <25aullal@derryfield.org>
2025-04-19 23:23:11 -0400

    Sim B Completed

--- Sim Project/animate/Board.java
+++ Sim Project/animate/Board.java
@@ -1,5 +1,6 @@
 package animate;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Dimension;
@@ -10,16 +11,132 @@ import java.awt.Shape;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+class Cannon {
+    private double x;
+    private double y;
+    private double theta;
+    private SoundClip cannonS;
+    private SoundClip wheelS;
+    private BufferedImage img;
+    private double muzzle_velocity;
+
+    public Cannon() {
+        cannonS = new SoundClip("media/cannon.wav");
+        wheelS = new SoundClip("media/wheel.wav");
+        cannonS.open();
+        wheelS.open();
+        try {
+            File imageFile = new File("media/sm_cannon.png");
+            img = ImageIO.read(imageFile);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        theta = 315.0;
+        x = 60;
+        y = 840;
+
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getTheta() {
+        return theta;
+    }
+
+    public double getVelocity() {
+        return muzzle_velocity;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setTheta(double theta) {
+        this.theta = theta;
+    }
+
+    public void setVelocity(double v) {
+        this.muzzle_velocity = v;
+    }
+
+    public void rotateCW() {
+        if (theta < 360) {
+            theta++;
+            wheelS.play();
+        } else {
+            System.out.println("Cannon is already horizontal");
+        }
+    }
+
+    public void rotateCCW() {
+        if (theta > 270) {
+            theta--;
+            wheelS.play();
+        } else {
+            System.out.println("Cannon is already vertical");
+        }
+    }
+
+    public void Fire() {
+        cannonS.play();
+    }
+
+    public void render(Graphics2D g2d) {
+        AffineTransform affineTransform = new AffineTransform();
+
+        if (img != null) {
+            double x_t = 15;
+            double y_t = img.getHeight() / 2.0;
+            double r = Math.toRadians(theta);
+
+            affineTransform.translate(x - x_t, y - y_t);
+            affineTransform.rotate(r, x_t, y_t);
+
+            g2d.drawImage(img, affineTransform, null);
+
+        } else {
+            g2d.setColor(Color.BLUE);
+            g2d.drawString("Unable to load image!", 25, 25);
+        }
+
+        int[] xCoords = { (int) x, (int) x - 40, (int) x + 40 };
+        int[] yCoords = { (int) y, (int) y + 40, (int) y + 40 };
+        g2d.setColor(Color.PINK);
+        g2d.fillPolygon(xCoords, yCoords, 3);
+        g2d.setColor(Color.BLACK);
+        g2d.drawPolygon(xCoords, yCoords, 3);
+
+        g2d.setColor(Color.BLUE);
+        g2d.fillOval((int) (x - 5), (int) (y - 5), 10, 10);
+
+    }
+
+}
 
 public class Board extends JPanel implements KeyListener {
     private final int B_WIDTH = 1600;
     private final int B_HEIGHT = 900;
     private final int SIDE_LEN = 150;
+    private Cannon cannon;
 
     /*
      * Constructor
      */
     public Board() {
+        cannon = new Cannon();
         setBackground(Color.CYAN);
         setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
 
@@ -40,6 +157,8 @@ public class Board extends JPanel implements KeyListener {
         g2d.drawLine(0, FLOOR, B_WIDTH, FLOOR);
         g2d.setColor(Color.GREEN);
         g2d.fillRect(0, FLOOR + 1, B_WIDTH, B_HEIGHT);
+
+        cannon.render(g2d);
     }
 
     @Override
@@ -50,10 +169,15 @@ public class Board extends JPanel implements KeyListener {
             System.out.println("Down arrow pressed");
         } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
             System.out.println("Right arrow pressed");
+            cannon.rotateCW();
+            repaint();
         } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
             System.out.println("Left arrow pressed");
+            cannon.rotateCCW();
+            repaint();
         } else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
             System.out.println("Spacebar pressed");
+            cannon.Fire();
         }
     }
 
